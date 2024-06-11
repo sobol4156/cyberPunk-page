f49a999f161a0fffd4af3436b313501a4c225e7b
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-12 00:41:04 +0300

    pages settings

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-import path from 'path';
+import {ghPages} from 'vite-plugin-gh-pages';
 
 export default defineConfig({
-  plugins: [vue()],
-
+  plugins: [
+    vue(),
+    ghPages({
+      branch: 'gh-pages',
+      dir:'dist'
+    }
+    )
+  ],
+  base: '/cyberPunk-page/' 
 });
