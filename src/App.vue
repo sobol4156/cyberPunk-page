47c48dbb8bc46d400ea29b094f83063c1226489f
Alex <147102432+sobol4156@users.noreply.github.com>
2024-06-13 01:17:18 +0300

    footer

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,12 @@
 <script setup>
+
 import Buy from './components/main/buy/Buy.vue';
 import Hp from './components/main/hp/Hp.vue';
 import Header from './components/header/Header.vue';
 import About from './components/main/about/About.vue';
 import Game from './components/main/game/Game.vue';
 import Hero from './components/main/hero/Hero.vue';
-
+import Footer from './components/footer/Footer.vue';
 
 </script>
 
@@ -19,7 +20,7 @@ import Hero from './components/main/hero/Hero.vue';
       <Hp/>
       <Buy/>
     </main>
-  
+    <Footer/>
 </template>
 
 <style scoped>
---
+++ src/components/footer/Footer.vue
@@ -0,0 +1,97 @@
+<script setup></script>
+
+<template>
+  <footer class="footer">
+    <div class="footer-up">
+      <div class="footer__container container">
+        <div class="footer__logos">
+          <img class="footer__logo-first" src="/image/footer/first.png" alt="" />
+          <img class="footer__logo-second" src="/image/footer/second.png" alt="" />
+        </div>
+        <div class="footer__nav">
+          <ul class="footer__list">
+            <li>
+              <span>Лицензия</span>
+            </li>
+            <li>
+              <span>Политика конфиденциальности</span>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </div>
+    <div class="footer-down">
+      <span>CD PROJEKT®, Cyberpunk®, Cyberpunk 2077®</span>
+    </div>
+  </footer>
+</template>
+
+<style lang="scss" scoped>
+.footer__container {
+  display: flex;
+  justify-content: space-between;
+  padding: 10px 20px;
+}
+.footer__logos {
+  display: flex;
+  gap: 32px;
+}
+.footer__nav {
+  display: flex;
+  align-items: center;
+}
+.footer__list {
+  padding: 0;
+  list-style-type: none;
+  display: flex;
+  gap: 32px;
+}
+.footer__list {
+  span {
+    font-family: Roboto;
+    font-size: 18px;
+    font-weight: 400;
+    line-height: 21.09px;
+    text-align: left;
+    cursor: pointer;
+  }
+}
+.footer-down {
+  background-color: black;
+  color: white;
+  text-align: center;
+  padding: 10px 0;
+  font-family: Roboto;
+  font-size: 12px;
+  font-weight: 400;
+  line-height: 14.06px;
+  text-align: center;
+}
+@media(width<967px){
+    .footer__container{
+        flex-direction: column;
+
+    }
+    .footer__logos{
+        flex-direction: column;
+        align-items: center;
+        gap: 12px;
+        .footer__logo-first{
+            width: 296px;
+            
+        }
+        .footer__logo-second{
+            width: 116px;
+        }
+    }
+    .footer__list{
+        flex-direction: column;
+       align-items: center;
+       gap: 15px;
+    }
+    .footer__nav{
+        margin-top: 20px;
+        justify-content: center;
+    }
+}
+</style>
